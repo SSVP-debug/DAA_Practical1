d384731e42003449e960f4a9132603e2e40f12d5
Shiva Shankara Vara Prasad <ssvp006@gmail.com>
2026-07-30 13:46:19 +0530

    Refactor SelectionSort.cpp for clarity
    Removed unnecessary comments and improved formatting.

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// Function to perform Selection Sort
+
 void selectionSort(int arr[], int n) {
-    // One by one move the boundary of the unsorted subarray
+    
     for (int i = 0; i < n - 1; i++) {
-        // Find the minimum element in the unsorted array
+        
         int min_idx = i;
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
-        // Swap the found minimum element with the first element of unsorted part
+        
         if (min_idx != i) {
             int temp = arr[i];
             arr[i] = arr[min_idx];
@@ -21,7 +21,7 @@ void selectionSort(int arr[], int n) {
     }
 }
 
-// Function to print the array
+
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
